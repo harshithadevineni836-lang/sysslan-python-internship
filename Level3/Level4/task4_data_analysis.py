684827e4adc53335ccc70419bcf3dc66d65fcc08
Devineni Lakshmi Venkata Harshitha <harshithadevineni836@gmail.com>
2026-06-09 08:19:23 +0530

    Completed Level 4 Tasks

---
+++ Level3/Level4/task4_data_analysis.py
@@ -0,0 +1,9 @@
+numbers = [10, 20, 30, 40, 50]
+
+total = sum(numbers)
+average = total / len(numbers)
+maximum = max(numbers)
+
+print("Total:", total)
+print("Average:", average)
+print("Maximum:", maximum)
